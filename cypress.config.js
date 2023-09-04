4119e616b755282665cf99d20229e567d3dd1b55
akrrrams <akins7559@gmail.com>
2023-09-04 14:42:52 -0400

    firs commit

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+
+    baseUrl: 'https://practice.cydeo.com/',
+    video: false,
+    retries: 2,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/testStructure.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+describe('Context: My First Tests', () => {
+    before(()=>{
+        //reun once before all test
+    })
+
+    beforeEach(() =>{
+        //run before each test case
+        cy.clearCookies;
+
+    })
+    after(()=> {
+            //similar to after class in testNG 
+    })
+
+    afterEach(()=> {
+        //similar to afterMeyhod in TestNG
+    })
+
+    it('opeing a web appliaction', ()=> {
+        cy.visit('registration_form');
+       // cy.get(':nth-child(8) > a').click();
+       // cy.get('.nav-link').click();
+    } )
+
+    it('Test 2', ()=> {
+    expect(false).to.equal(false);
+    })
+
+    it('Test 3', ()=> {
+        expect(false).not.to.equal(true);
+        })
+
+        it('Test 4', ()=> {
+            expect(5).to.equal(5);
+            })
+            it('Test 5', ()=> {
+                expect(true).to.equal('5'==5);
+                })
+
+})
